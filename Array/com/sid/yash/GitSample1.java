826c82ff5b53fcfa0824ffbc6a44cb455dd798e2
venu460 <50403740+venu460@users.noreply.github.com>
2019-07-05 19:40:27 +0530

    Deleted

--- Array/com/sid/yash/GitSample1.java
+++ Array/com/sid/yash/GitSample1.java
@@ -4,7 +4,7 @@ public class GitSample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("My name is siddiqui 123");
+
 System.out.println("venu changes code");
 System.out.println("venu changes code 123");
 
